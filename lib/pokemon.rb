685e1462873344a6ddcff9aa5f50ceb28bad4e18
Kirsten Werner <kirstenmwerner@gmail.com>
2019-03-22 23:19:33 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,14 +1,15 @@
 class Pokemon
   
-  attr_accessor :id, :name, :type, :db 
+  attr_accessor :id, :name, :type, :db, :hp 
   # database_connection = SQLite3::Database.new('db/my_database.db')
 
-  def initialize(name:, id:, type:, db:)
+  def initialize(name:, id:, type:, db:, hp: = nil)
     # Scraper.new(db)
     @id = id
     @name = name
     @type = type
     @db = db
+    @hp = hp
   end 
   
   def self.save(name, type, db)
@@ -18,7 +19,7 @@ class Pokemon
   def self.find(id, db)
     name = db.execute("SELECT name FROM pokemon WHERE pokemon.id = #{id}").flatten[0]
     type = db.execute("SELECT type FROM pokemon WHERE pokemon.id = #{id}").flatten[0]
-    new_pokemon = Pokemon.new(name: name, id: id, type: type, db: db)
+    new_pokemon = Pokemon.new(name: name, id: id, type: type, db: db, hp: = 60)
   end 
   
 end
