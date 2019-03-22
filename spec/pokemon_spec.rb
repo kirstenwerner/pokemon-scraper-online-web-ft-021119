07d66621ababd450ad85c78eb1757024c2362510
Kirsten Werner <kirstenmwerner@gmail.com>
2019-03-22 23:24:16 +0000

    Automatically backed up by Learn

--- spec/pokemon_spec.rb
+++ spec/pokemon_spec.rb
@@ -67,7 +67,7 @@ describe "Pokemon" do
 
     # Now we alter Magikarp's hp
     # xit "alters Magikarp's hp" do
-      magikarp.alter_hp(0, @db)
+      # magikarp.alter_hp(0, @db)
   #     expect(Pokemon.find(2, @db).hp).to eq(0)
   #   end
   end
